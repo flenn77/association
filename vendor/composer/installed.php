7e8cc2956fe1539d49e3604d4958b8918947ffc2
Steven Sivakaran <sivakaran19@hotmail.fr>
2022-11-02 16:56:56 +0100

    base du site (squelette)

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'association/animal',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'e2c78167f143ef24a6b237a67f87cfdb0be2c8a7',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'association/animal' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'e2c78167f143ef24a6b237a67f87cfdb0be2c8a7',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
